488199a1cee74d03fc12453cf675bce6022edaf2
kmarples1 <kmarples1@studentmail.nait.ca>
2017-11-22 20:27:27 -0700

    Added changes to BLL and WorkSchedule Library

--- Reporting/BLL/EmployeeSkillsController.cs
+++ Reporting/BLL/EmployeeSkillsController.cs
@@ -9,28 +9,51 @@ using WorkSchedule.System.DAL;
 
 namespace WorkSchedule.System.BLL
 {
+    //[DataObject]
+    //public class EmployeeSkillsController
+    //{
+    //    [DataObjectMethod(DataObjectMethodType.Select)]
+    //    public List<SkillStatus> GetSkillStatus()
+    //    {
+    //        using (var context = new WorkScheduleContext())
+    //        {
+    //            var results = from data in context.Skills
+    //                          from ski in data.EmployeeSkills
+    //                          orderby ski.Skill
+    //                          select new SkillStatus
+    //                          {
+    //                              Skill = data.Description,
+    //                              // Name = data.Employee.FirstName,
+    //                              // Phone = data.Phone,
+    //                              // YOE = data.YOE
+    //                          };
+    //            return results.ToList();
+    //        }
+    //    }
+
     [DataObject]
-    public class EmployeeSkillsController
+    public class EmployeeSkillController
     {
         [DataObjectMethod(DataObjectMethodType.Select)]
-        public List<SkillStatus> GetSkillStatus()
+        public List<SkillStatus> GetEmployeeSkills()
         {
             using (var context = new WorkScheduleContext())
             {
-                var results = from data in context.Skills
-                              from ski in data.EmployeeSkills
-                              orderby ski.Skill
-                              select new SkillStatus
-                              {
-                                  Skill = data.Description,
-                                  // Name = data.Employee.FirstName,
-                                  // Phone = data.Phone,
-                                  // YOE = data.YOE
-                              };
-                return results.ToList();
+                var result = from emp in context.Employees
+                             from data in emp.EmployeeSkills
+                             orderby data.Skill.Description, emp.LastName
+                             select new SkillStatus
+                             {
+                                 Skill = data.Skill.Description,
+                                 Name = emp.LastName,
+                                 Phone = emp.HomePhone,
+                                 //Level = data.Level,
+                                 //YOE = data.YearsOfExperience 
+                             };
+                return result.ToList();
             }
         }
-
-
     }
+
 }
+
